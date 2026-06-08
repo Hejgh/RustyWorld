1d8f27ae9490166623e018db194e21096d3bf596
Hejgh <105372476+Hejgh@users.noreply.github.com>
2026-06-08 00:47:32 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
+mod player;
+mod terrain;
 
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, player::spawn_player, terrain::generate_terrain))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
